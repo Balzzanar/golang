483f030680e22bd7465ddd16373f2819e58309de
Sphann <spann.johan@gmail.com>
2016-04-19 22:37:20 +0200

    Added support for json formated recept

--- sphann.se/main.go
+++ sphann.se/main.go
@@ -4,20 +4,69 @@ import (
 	"html/template"
 	"net/http"
 	"log"
-//	"strings"
+	"io/ioutil"
+	"encoding/json"
 )
 
 const LOGPATH string = "page.log"
 var templates = template.Must(template.ParseGlob("html/*"))
 
+type Ingredience struct {
+	Name string
+	Amount string
+}
+
+type Recept struct {
+	ID string
+	Name string
+	Ingrediences []Ingredience 
+	Making []string 
+	Notes []string
+	Image string
+}
+
 type Page struct {
 	Title string
+	Recepts []Recept 
+}
+
+/**
+ * loadRecepts
+ *
+ * Reads recepies from json formated files in a given dir.
+ */
+func loadRecepts() []Recept {
+	var recepts []Recept
+	files, err := ioutil.ReadDir("recept-files")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		b, _ := ioutil.ReadFile("recept-files/" + file.Name())
+		recepts = append(recepts, parseRecept(b))
+	}
+	return recepts
+}
+
+/**
+ * parseRecept
+ *
+ * Help function for loadRecepts, that parses the json recept.
+ */
+func parseRecept(raw []byte) Recept {
+	var recept = Recept{}
+	err := json.Unmarshal(raw, &recept)
+	if err != nil {
+		log.Println("error:", err)
+	}
+	return recept
 }
 
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	p := &Page{
-		Title: "This is a test page!",
+		Title: "Sphann, home page!",
+		Recepts: loadRecepts(),
 	}
 
     err := templates.ExecuteTemplate(w, "home", p)
@@ -41,7 +90,7 @@ func main() {
     http.Handle("/recources/", http.StripPrefix("/recources/", resources))
 	
 	http.HandleFunc("/", homeHandler)
-	
+
 	log.Printf("|Running...")
 	log.Fatal(http.ListenAndServe(":80", nil)) 
 }
